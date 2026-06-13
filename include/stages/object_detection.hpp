62b28a6a270d0e5700782ea33a4a4d8f67c53072
varunvn353 <varun.sh353@gmail.com>
2026-06-13 17:22:44 +0530

    Update object_detection.hpp

--- include/stages/object_detection.hpp
+++ include/stages/object_detection.hpp
@@ -1,22 +1,16 @@
 #pragma once
-
 #include "stages/stage.hpp"
 #include <vector>
-
 struct DetectedObject {
     cv::Rect bounding_box;
     double area;
 };
-
 class ObjectDetectionStage : public Stage {
 public:
     explicit ObjectDetectionStage(int min_area = 500, int max_area = 50000);
-
     void process(const cv::Mat& input, cv::Mat& output) override;
     std::string name() const override { return "Object Detection"; }
-
     const std::vector<DetectedObject>& lastDetections() const { return detections_; }
-
 private:
     int min_area_;
     int max_area_;
